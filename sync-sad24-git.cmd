8876e2304eb345ebdaa3cb9d83f5ffa953547232
Maslov Dmitry <dmitrii_m@inbox.ru>
2018-09-20 08:05:46 +0300

    Добавил скрипты синхронизации

--- sync-sad24-git.cmd
+++ sync-sad24-git.cmd
@@ -1,10 +1,10 @@
 cd c:\sad24.rf-c
-robocopy %userprofile%\Documents\Arduino\libraries\debug C:\sad24.rf-c\debug
-robocopy %userprofile%\Documents\Arduino\libraries\gprs2 C:\sad24.rf-c\gprs2
-robocopy %userprofile%\Documents\Arduino\libraries\mstr C:\sad24.rf-c\mstr
-robocopy %userprofile%\Documents\Arduino\libraries\TimerOne C:\sad24.rf-c\TimerOne
-robocopy %userprofile%\Documents\Arduino\libraries\worker C:\sad24.rf-c\worker
-robocopy %userprofile%\Documents\Arduino\sad24v2\sad24v2.ino C:\sad24.rf-c
+robocopy %userprofile%\Documents\Arduino\libraries\debug debug
+robocopy %userprofile%\Documents\Arduino\libraries\gprs2 gprs2
+robocopy %userprofile%\Documents\Arduino\libraries\mstr mstr
+robocopy %userprofile%\Documents\Arduino\libraries\TimerOne TimerOne
+robocopy %userprofile%\Documents\Arduino\libraries\worker worker
+robocopy %userprofile%\Documents\Arduino\sad24v2\sad24v2.ino sad24.rf-c
 git status
 git add *
 git commit -m %1
---
+++ sync-workarea.cmd
@@ -0,0 +1,7 @@
+cd c:\sad24.rf-c
+robocopy debug %userprofile%\Documents\Arduino\libraries\debug
+robocopy gprs2 %userprofile%\Documents\Arduino\libraries\gprs2
+robocopy mstr %userprofile%\Documents\Arduino\libraries\mstr
+robocopy TimerOne %userprofile%\Documents\Arduino\libraries\TimerOne
+robocopy worker %userprofile%\Documents\Arduino\libraries\worker
+                                  
