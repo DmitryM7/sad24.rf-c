b0fa72351d28c7abc7f64ce7005787aa100306a3
Maslov Dmitry <dmitrii_m@inbox.ru>
2018-12-02 16:47:48 +0300

    Добавил синхронизацию зависимых библиотек чтобы сразу можно было начать работу по кодированию устройства.

--- sync-workarea.cmd
+++ sync-workarea.cmd
@@ -2,6 +2,12 @@ cd c:\sad24.rf-c
 
 robocopy gprs2 %userprofile%\Documents\Arduino\libraries\gprs2
 robocopy mstr %userprofile%\Documents\Arduino\libraries\mstr
-robocopy TimerOne %userprofile%\Documents\Arduino\libraries\TimerOne
 robocopy worker %userprofile%\Documents\Arduino\libraries\worker
+
+robocopy TimerOne %userprofile%\Documents\Arduino\libraries\TimerOne
+robocopy DHT_sensor_library %userprofile%\Documents\Arduino\libraries\DHT_sensor_library
+robocopy bmp085 %userprofile%\Documents\Arduino\libraries\bmp085
+robocopy DS3231 %userprofile%\Documents\Arduino\libraries\DS3231
+robocopy LowPower %userprofile%\Documents\Arduino\libraries\LowPower
+
                                   
