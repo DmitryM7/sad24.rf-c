1900d612696fdd121fc22be0c8e8fbe760dfb706
Maslov Dmitry <dmitrii_m@inbox.ru>
2018-12-05 02:25:04 +0300

    Добавил в скрипт синхронизации синхронизацию модицированной библиотеки bmp085

--- sync-sad24-git.cmd
+++ sync-sad24-git.cmd
@@ -1,7 +1,11 @@
 cd c:\sad24.rf-c
 
+### Own library ###
 robocopy %userprofile%\Documents\Arduino\libraries\gprs2 gprs2
 robocopy %userprofile%\Documents\Arduino\libraries\mstr mstr
-robocopy %userprofile%\Documents\Arduino\libraries\TimerOne TimerOne
 robocopy %userprofile%\Documents\Arduino\libraries\worker worker
+
+### From other source libraries ###
+robocopy %userprofile%\Documents\Arduino\libraries\TimerOne TimerOne
+robocopy %userprofile%\Documents\Arduino\libraries\bmp085 bmp085
 
