5fd7051f9f9e8521ec59b76fe98196dcae9a3ba1
Maslov Dmitry <dmitrii_m@inbox.ru>
2018-12-14 22:55:40 +0300

    Добавил недостающие библиотеку MemoryFree. Скорректировал скрипт синхронизации.

--- sync-sad24-git.cmd
+++ sync-sad24-git.cmd
@@ -1,11 +1,15 @@
-cd c:\sad24.rf-c
-
-### Own library ###
+########### Own library ############
 robocopy %userprofile%\Documents\Arduino\libraries\gprs2 gprs2
 robocopy %userprofile%\Documents\Arduino\libraries\mstr mstr
 robocopy %userprofile%\Documents\Arduino\libraries\worker worker
+###################################
 
 ### From other source libraries ###
-robocopy %userprofile%\Documents\Arduino\libraries\TimerOne TimerOne
 robocopy %userprofile%\Documents\Arduino\libraries\bmp085 bmp085
+robocopy %userprofile%\Documents\Arduino\libraries\DHT_sensor_library DHT_sensor_library
+robocopy %userprofile%\Documents\Arduino\libraries\TimerOne TimerOne
+robocopy %userprofile%\Documents\Arduino\libraries\LowPower LowPower
+robocopy %userprofile%\Documents\Arduino\libraries\MemoryFree-master MemoryFree-master
+robocopy %userprofile%\Documents\Arduino\libraries\DS3221 DS3221
+####################################
 
