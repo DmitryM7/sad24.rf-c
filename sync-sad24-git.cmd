0681847e8f4c6bd478e35dc38abb238b026cc865
Maslov Dmitry <dmitrii_m@inbox.ru>
2019-05-19 20:34:54 +0300

    Скорректировал вывод комментариев в скриптах. Перевел кодировку на UTF-8

--- sync-sad24-git.cmd
+++ sync-sad24-git.cmd
@@ -1,15 +1,15 @@
-########### Own library ############
+REM ########### Own library ########################################
 robocopy %userprofile%\Documents\Arduino\libraries\gprs2 gprs2
 robocopy %userprofile%\Documents\Arduino\libraries\mstr mstr
 robocopy %userprofile%\Documents\Arduino\libraries\worker worker
-###################################
+REM #################################################################
 
-### From other source libraries ###
+REM ### From other source libraries ###
 robocopy %userprofile%\Documents\Arduino\libraries\bmp085 bmp085
 robocopy %userprofile%\Documents\Arduino\libraries\DHT_sensor_library DHT_sensor_library
 robocopy %userprofile%\Documents\Arduino\libraries\TimerOne TimerOne
 robocopy %userprofile%\Documents\Arduino\libraries\LowPower LowPower
 robocopy %userprofile%\Documents\Arduino\libraries\MemoryFree-master MemoryFree-master
 robocopy %userprofile%\Documents\Arduino\libraries\DS3231 DS3231
-####################################
+REM ####################################
 
