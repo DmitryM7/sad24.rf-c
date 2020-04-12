7aaa94294b5b52ecf357033eb3c2766e8ea5598c
Maslov Dmitry <dmitrii_m@inbox.ru>
2020-04-12 14:05:19 +0300

    Разбил на блоки

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,23 +1,55 @@
-#define ECHO_PIN 13
+/***************************************************************
+ * Приложение измеряет уровень воды                            *
+ * и если он ниже заданного, то запрещает включение насоса.    *
+ ***************************************************************/
+
+ #include <worker.h>
+
+/** Задаем параметры подключения УЗД **/
+#define ECHO_PIN 10
 #define TRIG_PIN 3
 
+/** Задаю пороговые значения в сантиметрах **/
+
+
+workerInfo _water;
+workerInfo _light;
+
+
+int getDistance() {
+    int duration, cm;     
+    digitalWrite(TRIG_PIN, LOW); 
+    delayMicroseconds(2); 
+    digitalWrite(TRIG_PIN, HIGH); 
+    delayMicroseconds(55); 
+    digitalWrite(TRIG_PIN, LOW); 
+    duration = pulseIn(ECHO_PIN, HIGH); 
+    cm = duration / 58;
+    return cm;
+}
  
 void setup() { 
     Serial.begin (19200); 
     pinMode(TRIG_PIN, OUTPUT); 
     pinMode(ECHO_PIN, INPUT); 
+    pinMode(LED_BUILTIN, OUTPUT);
+
 } 
  
 void loop() { 
-    int duration, cm; 
+   int vCurrDistance = getDistance();
+   worker _worker(0);    
+   digitalWrite(LED_BUILTIN,HIGH);
+  
+   Serial.print(vCurrDistance); Serial.println(" cm");   
 
-    
-    digitalWrite(TRIG_PIN, LOW); 
-    delayMicroseconds(2); 
-    digitalWrite(TRIG_PIN, HIGH); 
-    delayMicroseconds(10); 
-    digitalWrite(TRIG_PIN, LOW); 
-    duration = pulseIn(ECHO_PIN, HIGH); 
-    cm = duration / 58;
-    Serial.print(cm); Serial.println(" cm");   delay(1000);
+   if (vCurrDistance>150) {
+      _worker.startWater();
+   } else {
+      _worker.stopWater();
+   };
+   delay(1000);
+   digitalWrite(LED_BUILTIN,LOW);
+   delay(1000);
+   
 }
