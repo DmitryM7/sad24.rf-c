f1e180fd5a793d370679d776beb839cf73bad8b5
Maslov Dmitry <dmitrii_m@inbox.ru>
2020-07-06 08:32:46 +0300

    Добавил возможность подключения ведомого устройства. Предполагается, что пользователь захочет поливать участок и дом от одного насоса (возможно это крайне наивное предположение).

--- WaterSensor/WaterSensor.ino
+++ WaterSensor/WaterSensor.ino
@@ -1,6 +1,7 @@
 #include <avr/wdt.h>
 #include <debug.h>
 #include <worker.h>
+#include <mstr.h>
 /************************************************************************************************
  * Пример кода для посчета количества протекующей жидкости                                      *
  * int Calc;                               
@@ -17,16 +18,15 @@
          
 volatile int NbTopsFan; //measuring the rising edges of the signal
 volatile unsigned long int mPrevFlow = 0;
+volatile bool mCanEnablePump=true;
+volatile unsigned long int mWirePumpOn=0,mWirePumpOff=0;
 bool isWater = false;  
+unsigned long int mLastWireOffSend=0,mLastWireOnnSend=0;
 
 workerInfo _water;
 workerInfo _light;
 
 
-#define CRITICAL_PREASURE 3       // Критическое давление при котором устройсво отключится без учета  потока.
-#define MAX_PREASURE 2.5          // Давление при котором устройство отключится если нет потока
-#define MIN_PREASURE 2.1          // В случае если давление в системе упадет меньше указанного, то устройство вклчючит насос
-#define FLOW_DIFF 50              // Время в течение которого устройство опеделяет отсутствие потока.
 
 #define HAS_ULTRASONIC_SENSOR 0
 #define LEVEL_WATER_TOP 75        // Уровень при котором нужно включить насос для откачки. Другими словами, если вода ближе, чем указанная здесь цфира, то включаем насосо.
@@ -36,7 +36,24 @@ workerInfo _light;
 
 #define ECHO_PIN   10           // ECHO  pin УЗ-сенсора
 #define TRIG_PIN   3            // TRIG  pin УЗ-сенсора
-#define TRIG_DELAY 50           // DELAY задержка перед опроса сигнала
+#define TRIG_DELAY 40           // DELAY задержка перед опроса сигнала
+  
+#define WIRE_SERVER 1
+#define WIRE_ADDRESS 7
+#define WIRE_REPEAT_TIMEOUT 3000
+#define WORD_LENGTH 30
+
+#if WIRE_SERVER>0
+  #define CRITICAL_PREASURE 2.9        // Критическое давление при котором устройсво отключится без учета  потока.
+  #define MAX_PREASURE 2.15          // Давление при котором устройство отключится если нет потока
+  #define MIN_PREASURE 2.10          // В случае если давление в системе упадет меньше указанного, то устройство вклчючит насос
+  #define FLOW_DIFF 40              // Время в течение которого устройство опеделяет отсутствие потока.
+#else
+  #define CRITICAL_PREASURE 3.5        // Критическое давление при котором устройсво отключится без учета  потока.
+  #define MAX_PREASURE 2.2          // Давление при котором устройство отключится если нет потока
+  #define MIN_PREASURE 1.9          // В случае если давление в системе упадет меньше указанного, то устройство вклчючит насос
+  #define FLOW_DIFF 50              // Время в течение которого устройство опеделяет отсутствие потока.
+#endif
 
 worker _worker(0);    
 
@@ -64,7 +81,12 @@ float getCurrPreasure() {
   unsigned int v;
   float ret;
   v  = analogRead(A0);
+
   ret = 2 * (float)v * 5 / 1024;
+
+  //Округляем до 100тых
+  ret=floor(ret * 100) / 100;
+  
   return ret < 0 ? 0 : ret;  
 }
 
@@ -89,50 +111,101 @@ void setup()
   _worker.stopWater();
   _worker.stopLight();
 
+  
+
+  #if WIRE_SERVER==0
+    Wire.begin();
+  #else
+    Wire.begin(WIRE_ADDRESS);
+    Wire.onReceive(receiveEvent);
+  #endif
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
   wdt_enable (WDTO_8S);
   
 } 
 void loop ()    
 {
 
-  float mCurrPreasure = getCurrPreasure(), 
-        mDistance     = getDistance(),
-        mCurrFlowTimeDiff;
+  float mCurrPreasure, 
+        mDistance     = getDistance();
+   unsigned long  mCurrFlowTimeDiff=getPrevFlowDiff();
+
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN,LOW);
+
+  mCurrPreasure = getCurrPreasure();
 
   #if IS_DEBUG>1
-      //Serial.print(mCurrPreasure); Serial.print(F(" and flow ")); Serial.print(getPrevFlowDiff()); Serial.println(); Serial.flush();
+      Serial.print(mCurrPreasure); Serial.print(F(" and flow ")); Serial.print(mCurrFlowTimeDiff); Serial.println(); Serial.flush();
   #endif 
+  
+ #if WIRE_SERVER>0                  
 
- /******************************************************************
-  * Проверка №1.                                                  *
-  * Проверяем, что уровень допустим для работы насоса. Если вода   *
-  * ушла дальше, то не разрешаем включать насос. При этом включаем *
-  * запрещающий инидкатор.                                         *
-  ******************************************************************/
+    if (!isWater) {
+        //Если давление позволяет, то включаем насос
+        if (mCurrPreasure  < (CRITICAL_PREASURE - 0.02)) { //Как временное решение делаю поправку на 0.02, т.к. датчик скачет в этом диапазоне  
 
+          if(mWirePumpOn > mWirePumpOff && millis() - mWirePumpOn < (WIRE_REPEAT_TIMEOUT + 1000)) {
+            #if IS_DEBUG>0
+              Serial.println(F("ONN by WIRE")); Serial.flush();
+            #endif
+           _worker.startWater();
+          }; 
+       };
+
+       //Если получили сигнал на отключение, то отключаем насос, или не получили сигнал на включение. Видимо ведомый погиб.
+       if(mWirePumpOff > mWirePumpOn || millis() - mWirePumpOn > (WIRE_REPEAT_TIMEOUT + 1000)) {
+        #if IS_DEBUG>0
+          Serial.println(F("OFF by WIRE")); Serial.flush();
+        #endif
+        _worker.stopWater();
+      };            
+    };
+
+ #endif
+
+  
   #if HAS_ULTRASONIC_SENSOR>0
 
    if (mDistance>LEVEL_WATER_BOTTOM) {
-      _worker.stopWater();
-      _worker.startLight();
-      isWater=false;
-      return;
-    };
+    #ifdef IS_DEBUG
+     Serial.println(F("Lower water level. Pump disable.")); Serial.flush();
+    #endif
+    mCanEnablePump=false
+   } else {
+    mCanEnablePump=true;
+   };
 
-    _worker.stopLight(); // Отключаем запрешающий индикатор.
   #endif
- 
+
  /**************************************************************
   * Проверка №2.                                              *
   * Нет потока больше заданного интервала - отключаем воду.    *
   *************************************************************/
 
- if ((mCurrPreasure = getCurrPreasure()) >= MAX_PREASURE && (mCurrFlowTimeDiff = getPrevFlowDiff()) >= FLOW_DIFF && isWater) {
+ if (mCurrPreasure >= MAX_PREASURE && (mCurrFlowTimeDiff = getPrevFlowDiff()) >= FLOW_DIFF && isWater) {
 
   #ifdef IS_DEBUG
     Serial.print(F("Max preasure: ")); Serial.print(mCurrPreasure); Serial.print(F(" and no flow: ")); Serial.print(mCurrFlowTimeDiff); Serial.println(F(" - power off."));  Serial.flush();
    #endif
     _worker.stopWater();
+
+    #if WIRE_SERVER==0
+    if (millis() - mLastWireOffSend > WIRE_REPEAT_TIMEOUT) {
+      char wWord[WORD_LENGTH];
+      sprintf_P(wWord, PSTR("OFF|p:%lu|f:%lu|nf"),(unsigned long)(mCurrPreasure*100),mCurrFlowTimeDiff);
+      Wire.beginTransmission(WIRE_ADDRESS);
+      Wire.write(wWord);
+      Wire.endTransmission();  
+      mLastWireOffSend=millis();
+      #ifdef IS_DEBUG
+        Serial.print(F("Wire:")); Serial.println(wWord); Serial.flush();
+      #endif
+    }
+    #endif
     isWater = false;
   };
 
@@ -141,14 +214,32 @@ void loop ()
   * Достигли критического значения по давлению.          *
   * Отключаем насос вне зависимости от других параметров *
   ********************************************************/
-  if ((mCurrPreasure = getCurrPreasure()) >= CRITICAL_PREASURE) {
+  if (mCurrPreasure >= CRITICAL_PREASURE) {
 
    #ifdef IS_DEBUG
      Serial.print(F("!!! Critical preasure: ")); Serial.print(mCurrPreasure); Serial.print(F("- power off !!!")); Serial.print(F("FLOW: ")); Serial.println(getPrevFlowDiff()); Serial.flush();
    #endif
 
-    _worker.stopWater();
+   #if WIRE_SERVER==0
+    if (millis() - mLastWireOffSend > WIRE_REPEAT_TIMEOUT) {
+      char wWord[WORD_LENGTH];
+      sprintf_P(wWord, PSTR("OFF|p:%lu|f:%lu|cp"),(unsigned long)(mCurrPreasure*100),999999);
+      Wire.beginTransmission(WIRE_ADDRESS);
+      Wire.write(wWord);
+      Wire.endTransmission();  
+
+      mLastWireOffSend=millis();
+      
+      #ifdef IS_DEBUG
+        Serial.print(F("Wire:")); Serial.println(wWord); Serial.flush();
+      #endif
+    }
+    #endif
+
+
+   _worker.stopWater();
     isWater = false;
+    mCanEnablePump=false;
   };
 
   /**************************************************   
@@ -157,16 +248,66 @@ void loop ()
    * Включаем его.                                  *
    **************************************************/
 
-  if ((mCurrPreasure = getCurrPreasure()) < MIN_PREASURE && !isWater) {
+  if ((mCurrPreasure = getCurrPreasure()) < MIN_PREASURE) {
 
-   #ifdef IS_DEBUG
-    Serial.print(F("Min preasure: ")); Serial.print(mCurrPreasure); Serial.println(F(" - power on.")); Serial.flush();
-   #endif
-
-    _worker.startWater();
-    isWater = true;
+    if (!isWater) {
+      #ifdef IS_DEBUG
+        Serial.print(F("Min preasure: ")); Serial.print(mCurrPreasure); Serial.println(F(" - power on.")); Serial.flush();
+      #endif   
+       _worker.startWater();
+       isWater = true;
+    };
+   
   };
+
+  /**************************************************************
+   * Если у ведомого включена вода, то сообщаем об этом мастеру 
+   **************************************************************/
+
+   #if WIRE_SERVER==0
+    if (isWater) {
+      char wWord[WORD_LENGTH];
+      sprintf_P(wWord, PSTR("ONN|p:%lu|f:%lu"),(unsigned long)(mCurrPreasure*100),mCurrFlowTimeDiff);    
+      Wire.beginTransmission(WIRE_ADDRESS);
+      Wire.write(wWord);
+      Wire.endTransmission();  
+      mLastWireOnnSend=millis();
+      #ifdef IS_DEBUG
+        Serial.print(F("Wire:")); Serial.println(wWord); Serial.flush();
+      #endif   
+    };
+   #endif
 
   wdt_reset();
 
+}
+
+void receiveEvent(int howMany) {
+ char vAnswer[WORD_LENGTH];
+ char vNeedStr[4];
+ byte vCount=0;
+ mstr _mstr;
+
+ _mstr.emptyBuffer(vAnswer,sizeof(vAnswer));
+
+ 
+ while (Wire.available()) {
+  char c = Wire.read();
+     if (vCount<sizeof(vAnswer)) {
+        vAnswer[vCount]=c;
+        vCount++;
+     };
+ };
+
+ #if IS_DEBUG>0
+   Serial.print(F("Wire rec: ")); Serial.println(vAnswer); Serial.flush();
+ #endif
+
+ strcpy_P(vNeedStr, PSTR("ONN"));
+
+ if (_mstr.indexOf(vAnswer,vNeedStr)>-1) {
+   mWirePumpOn=millis();
+ } else {
+   mWirePumpOff=millis();
+ };
 }
